ffd1b1f5d9ec8a7f017848d77e955c53e3401625
SDM30 <simondiaz@javeriana.edu.co>
2025-03-22 15:06:21 -0500

    cambios para que el cliente pueda generar JAR

--- src/main/java/org/sistemasdistribuidos/MainCliente.java
+++ src/main/java/org/sistemasdistribuidos/MainCliente.java
@@ -22,6 +22,7 @@ public class MainCliente {
         int option = -1;
 
         try {
+            //Direccion IP del servidor, de lo contrario si se ejecuta como proceso dejar en 0.0.0.0
             Registry registry = LocateRegistry.getRegistry("0.0.0.0", 1099);
             iMyUBER interUBER = (iMyUBER) registry.lookup("ObjetoRemotoMyUBER");
 
