b6e7eaa6ec8e43efc9d8c9c384d6820f7b557a3f
SDM30 <simondiaz@javeriana.edu.co>
2025-03-21 19:01:14 -0500

    Estructura cliente

---
+++ src/main/java/org/sistemasdistribuidos/MainCliente.java
@@ -0,0 +1,85 @@
+package org.sistemasdistribuidos;
+
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.Scanner;
+
+/*
+* Autores: Juan Luis Ardila, Simon Diaz y Melissa Ruiz
+ */
+public class MainCliente {
+    public static void main(String[] args) {
+        // Colores CLI
+        final String RESET = "\u001B[0m";
+        final String ROJO = "\u001B[31m";
+        final String VERDE = "\u001B[32m";
+        final String AZUL = "\u001B[34m";
+
+        // Leer input del usuario
+        Scanner scanner = new Scanner(System.in);
+        int option = -1;
+
+        try {
+            Registry registry = LocateRegistry.getRegistry("0.0.0.0", 1099);
+            iMyUBER interUBER = (iMyUBER) registry.lookup("ObjetoRemotoMyUBER");
+
+            // Interfaz de consola
+            do {
+                System.out.println("Bienvenido usuario a myUBER");
+                System.out.println("1. Registrar usuario");
+                System.out.println("2. Otra opción");
+                System.out.println("3. Otra opción");
+                System.out.println("4. Salir");
+                System.out.print("Opción: ");
+
+                if (scanner.hasNextInt()) {
+                    option = scanner.nextInt();
+                    scanner.nextLine(); // Consumir el salto de línea pendiente
+
+                    switch (option) {
+                        case 1:
+                            System.out.print(AZUL + "Ingresa el nombre del usuario: " + RESET);
+                            String nombre = scanner.nextLine();
+
+                            System.out.print(AZUL + "Ingresa el número de teléfono del usuario: " + RESET);
+                            if (scanner.hasNextLong()) {
+                                long telefono = scanner.nextLong();
+                                scanner.nextLine(); // Consumir el salto de línea pendiente
+                                if (interUBER.registrarUsuario(nombre, telefono)) {
+                                    System.out.println(VERDE + "Registro exitoso" + RESET);
+                                } else {
+                                    System.out.println(ROJO + "El usuario no existe o algún dato no corresponde" + RESET);
+                                }
+                            } else {
+                                System.out.println(ROJO + "Número de teléfono inválido." + RESET);
+                                scanner.nextLine(); // Limpiar la entrada inválida
+                            }
+                            break;
+                        case 2:
+                            // Lógica para la opción 2
+                            System.out.println ( VERDE + interUBER.consutarTiposServicio () + RESET);
+                            break;
+                        case 3:
+                            // Lógica para la opción 3
+                            break;
+                        case 4:
+                            System.out.println("Saliendo del sistema.");
+                            break;
+                        default:
+                            System.out.println(ROJO + "Opción inválida" + RESET);
+                            break;
+                    }
+                } else {
+                    System.out.println(ROJO + "Entrada inválida. Por favor, ingresa un número." + RESET);
+                    scanner.nextLine(); // Limpiar la entrada inválida
+                }
+            } while (option != 4);
+        } catch (RemoteException | NotBoundException e) {
+            System.out.println("Error: " + e);
+        } finally {
+            scanner.close();
+        }
+    }
+}
